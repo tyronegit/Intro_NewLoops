6bdfda3a65aa2542b9ef30b9b7d2c6797a0bfd3e
T.Atkins <43272999+tyronegit@users.noreply.github.com>
2024-04-27 20:22:34 -0400

    formatted code

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
         for (double rate = 2.0; rate <= 5.0; rate++) {
             double interestAmount = calculateInterest ( 10000.0, rate );
-            System.out.println ( "10,000 at " + rate + "% interest = : $" + interestAmount );
+            System.out.println ( "$10,000 at " + rate + "% interest = : $" + interestAmount );
         }
 
 
@@ -21,7 +21,10 @@ public class Main {
         // of a percent each time, meaning 0.25 percent.
         for (double rate = 7.5; rate <= 10; rate += 0.25) {
             double interest = calculateInterest ( 100.0, rate );
-            System.out.println ( "Interest at " + rate + "% : $" + interest );
+            if (interest > 8.5) {
+                break;
+            }
+            System.out.println ( "$100.00 at " + rate + "% = $" + interest );
         }
     }
 
